a8c3b21db802fe91dfc8aef98b87b1e4d713dabe
Chris Llanwarne <cjllanwarne@gmail.com>
2019-05-31 17:13:10 -0400

    Hmmm...2...?

--- scripts/perf/delete_instance/delete_instance.sh
+++ scripts/perf/delete_instance/delete_instance.sh
@@ -12,5 +12,9 @@ docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault re
 
 # The cloning itself sometimes takes a long time and the clone command errors out when that happens.
 # Instead use the --async flag in the clone command above and then explicitly wait for the operation to be done. Timeout 15 minutes
-docker run --name perf_sql_delete_gcloud_${BUILD_NUMBER} -v "$(pwd)"/mnt:$DOCKER_ETC_PATH -e DOCKER_ETC_PATH=${DOCKER_ETC_PATH} --rm google/cloud-sdk:slim /bin/bash -c "\
-    gcloud --project broad-dsde-cromwell-perf compute instances delete ${CROMWELL_INSTANCE_NAME} --zone=us-central1-c -q"
+docker run --name perf_sql_delete_gcloud_${BUILD_NUMBER} \
+  -v "$(pwd)"/mnt:$DOCKER_ETC_PATH \
+  -e DOCKER_ETC_PATH="${DOCKER_ETC_PATH}" \
+  -e CROMWELL_INSTANCE_NAME="${CROMWELL_INSTANCE_NAME}" \
+  --rm \
+  google/cloud-sdk:slim /bin/bash -c "gcloud --project broad-dsde-cromwell-perf compute instances delete ${CROMWELL_INSTANCE_NAME} --zone=us-central1-c -q"
